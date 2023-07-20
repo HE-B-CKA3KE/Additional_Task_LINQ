c4414d8bec7650e5d1dab82275d5e7d65aa9d9c5
HE-B-CKA3KE <80928455+HE-B-CKA3KE@users.noreply.github.com>
2023-07-21 01:15:14 +0300

    AdditionalTask with Linq created

---
+++ AdditionalTask_LINQ/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+ class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<string> queueNames = new Queue<string>();
+        Random random = new Random();
+         string[] names = { "Alice", "Bob", "Charlie", "David", "Emma", "Frank", "Grace", "Henry", "Isabella", "John" };
+         for (int i = 0; i < 10; i++)
+        {
+            int randomIndex = random.Next(names.Length);
+            queueNames.Enqueue(names[randomIndex]);
+        }
+         Console.WriteLine("Random Names Queue:");
+        foreach (string name in queueNames)
+        {
+            Console.WriteLine(name);
+        }
+         Console.ReadLine();
+    }
+}
